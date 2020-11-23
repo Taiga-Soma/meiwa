4dbfd2ff23a2b55e2838ebebd2f8c80f9bddad96
Taiga-Soma <allvictory111@gmail.com>
2020-11-23 11:01:56 +0900

    パスワード忘れた画面css

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  
   has_many :homes
 
   with_options presence: true do
